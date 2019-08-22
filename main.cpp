6151c61e997ec99af7b158885339593c591f2d61
Augs Machado <augustomachadoramos@gmail.com>
2019-08-22 18:13:28 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 # include <iostream>
 # include <cstdlib>
-# include <ctime>
 # include <stdlib.h>
+# include <regex>
 using namespace std;
 
 #define SIZEBOARD 9
@@ -9,34 +9,31 @@ using namespace std;
 #define false 0
 
 void inicializeBoard(char board[]);
+void printBoard(char board[]);
 int isItASequenceUser(char board[]);
 void systemReact(char board[], int choiceUser);
+bool userChoiceIsValid (char board[], int choiceUser);
 
 
 // user plays with 'x' and system with 'o'
 int main () {
     char board[SIZEBOARD];
-    int choiceUser = -1;
+    int choiceUser;
 
     inicializeBoard(board);
 
     while (choiceUser != 0) {
+        //system ("CLS");
+        printBoard(board);
+        cout << endl << "Choose a position:" << endl;
         cin >> choiceUser;
         systemReact(board, choiceUser);
-
-        for(int i=0; i < SIZEBOARD; i++){
-            cout << board[i] << " ";
-            if ((i+1)%3 == 0) cout << endl;
-        }
-    }
-    for(int i=0; i < SIZEBOARD; i++){
-        cout << board[i] << " ";
-        if ((i+1)%3 == 0) cout << endl;
     }
     
     return 0;
 }
 
+/* The function inicialize the board with asterisk */
 void inicializeBoard (char board[]){
     int options = 1;
 
@@ -45,152 +42,180 @@ void inicializeBoard (char board[]){
     }
 }
 
-int isItASequenceUser(char board[]){
-    if(board[0] == 'x' and board[1] == 'x' and board[2] == '*') {
-        board[2] = 'o';
-        return true;
+/* The function print the board */
+void printBoard(char board[]){
+    for(int i=0; i < SIZEBOARD; i++){
+        
+        /* If the position contains an asterisk, prints the position number.
+            Else print the content, this is 'x' or 'o' */
+        if(board[i] == '*') {
+            cout << i+1 << " ";
+        } else {
+            cout << board[i] << " ";
+        }
+        
+        if ((i+1)%3 == 0) cout << endl;
     }
-    
-    if(board[4] == 'x' and board[5] == 'x' and board[3] == '*') {
-        board[3] = 'o';
-        return true;
-    }
-    
-    if(board[6] == 'x' and board[7] == 'x' and board[8] == '*') {
-        board[8] = 'o';
-        return true;
-    }
-    
-    if(board[0] == 'x' and board[3] == 'x' and board[6] == '*') {
-        board[6] = 'o';
-        return true;
-    }
-    
-    if(board[4] == 'x' and board[7] == 'x' and board[1] == '*') {
-        board[1] = 'o';
-        return true;
-    }
-    
-    if(board[2] == 'x' and board[5] == 'x' and board[8] == '*') {
-        board[8] = 'o';
-        return true;
-    }
-    
-    if(board[2] == 'x' and board[4] == 'x' and board[6] == '*') {
-        board[6] = 'o';
-        return true;
-    }
-    
-    if(board[4] == 'x' and board[8] == 'x' and board[0] == '*') {
-        board[0] = 'o';
-        return true;
-    }
-    
-    return false;
 }
 
 void systemReact(char board[], int choiceUser) {
+    bool choiceIsValid;
+    
     switch(choiceUser){
         case 1:
-            board[0] = 'x';
-            if (board[1] == '*') {
-                board[1] = 'o';
-                break;
-            } else if (board[3] == '*') {
-                board[3] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[1] != 'x' && board[1] != 'o') {
+                    board[1] = 'o';
+                    break;
+                } else if (board[3] != 'x' && board[3] != 'o') {
+                    board[3] = 'o';
+                    break;
+                }
             }
+            
         case 2:
-            board[1] = 'x';
-            if (board[0] == '*') {
-                board[0] = 'o';
-                break;
-            } else if (board[2] == '*') {
-                board[2] = 'o';
-                break;
-            } else if (board[4] == '*') {
-                board[4] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[0] != 'x' && board[0] != 'o') {
+                    board[0] = 'o';
+                    break;
+                } else if (board[2] != 'x' && board[2] != 'o') {
+                    board[2] = 'o';
+                    break;
+                } else if (board[4] != 'x' && board[4] != 'o') {
+                    board[4] = 'o';
+                    break;
+                }
             }
+            
         case 3:
-            board[2] = 'x';
-            if (board[1] == '*') {
-                board[1] = 'o';
-                break;
-            } else if (board[5] == '*') {
-                board[5] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[1] != 'x' && board[1] != 'o') {
+                    board[1] = 'o';
+                    break;
+                } else if (board[5] != 'x' && board[5] != 'o') {
+                    board[5] = 'o';
+                    break;
+                }
             }
+            
         case 4:
-            board[3] = 'x';
-            if (board[0] == '*') {
-                board[0] = 'o';
-                break;
-            } else if (board[4] == '*') {
-                board[4] = 'o';
-                break;
-            } else if (board[6] == '*') {
-                board[6] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[0] != 'x' && board[0] != 'o') {
+                    board[0] = 'o';
+                    break;
+                } else if (board[4] != 'x' && board[4] != 'o') {
+                    board[4] = 'o';
+                    break;
+                } else if (board[6] != 'x' && board[6] != 'o') {
+                    board[6] = 'o';
+                    break;
+                }
             }
+            
         case 5:
-            board[4] = 'x';
-            if (board[0] == '*') {
-                board[0] = 'o';
-                break;
-            } else if (board[2] == '*') {
-                board[2] = 'o';
-                break;
-            } else if (board[6] == '*') {
-                board[6] = 'o';
-                break;
-            } else if (board[8] == '*') {
-                board[8] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[0] != 'x' && board[0] != 'o') {
+                    board[0] = 'o';
+                    break;
+                } else if (board[2] != 'x' && board[2] != 'o') {
+                    board[2] = 'o';
+                    break;
+                } else if (board[6] != 'x' && board[6] != 'o') {
+                    board[6] = 'o';
+                    break;
+                } else if (board[8] != 'x' && board[8] != 'o') {
+                    board[8] = 'o';
+                    break;
+                }
             }
+            
         case 6:
-            board[5] = 'x';
-            if (board[2] == '*') {
-                board[2] = 'o';
-                break;
-            } else if (board[4] == '*') {
-                board[4] = 'o';
-                break;
-            } else if (board[8] == '*'){
-                board[8] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[2] != 'x' && board[2] != 'o') {
+                    board[2] = 'o';
+                    break;
+                } else if (board[4] != 'x' && board[4] != 'o') {
+                    board[4] = 'o';
+                    break;
+                } else if (board[8] != 'x' && board[8] != 'o'){
+                    board[8] = 'o';
+                    break;
+                }
             }
+            
         case 7:
-            board[6] = 'x';
-            if (board[3] == '*') {
-                board[3] = 'o';
-                break;
-            } else if (board[7] == '*') {
-                board[7] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[3] != 'x' && board[3] != 'o') {
+                    board[3] = 'o';
+                    break;
+                } else if (board[7] != 'x' && board[7] != 'o') {
+                    board[7] = 'o';
+                    break;
+                }
             }
+            
         case 8:
-            board[7] = 'x';
-            if (board[4] == '*') {
-                board[4] = 'o';
-                break;
-            } else if (board[6] == '*') {
-                board[6] = 'o';
-                break;
-            } else if (board[8] == '*'){
-                board[8] = 'o';
-                break;
+           choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[4] != 'x' && board[4] != 'o') {
+                    board[4] = 'o';
+                    break;
+                } else if (board[6] != 'x' && board[6] != 'o') {
+                    board[6] = 'o';
+                    break;
+                } else if (board[8] != 'x' && board[8] != 'o'){
+                    board[8] = 'o';
+                    break;
+                }
             }
+            
         case 9:
-            board[8] = 'x';
-            if (board[5] == '*') {
-                board[5] = 'o';
-                break;
-            } else if (board[7] == '*') {
-                board[7] = 'o';
-                break;
+            choiceIsValid = userChoiceIsValid(board, choiceUser);
+            
+            /* System reaction */
+            if (choiceIsValid == true) {
+                if (board[5] != 'x' && board[5] != 'o') {
+                    board[5] = 'o';
+                    break;
+                } else if (board[7] != 'x' && board[7] != 'o') {
+                    board[7] = 'o';
+                    break;
+                }
             }
+            
         default:
             break;
+    }
+}
+
+bool userChoiceIsValid (char board[], int choiceUser) {
+    if (board[choiceUser-1] != 'x' && board[choiceUser-1] != 'o'){
+        board[choiceUser-1] = 'x';
+        return true;
+    } else {
+        return false;
     }
 }
